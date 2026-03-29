db501cf2e1cb751a0b288fc3188239099eee6a87
Chris Vlamis <christosvlamis1@gmail.com>
2026-03-29 19:51:39 +0300

    Polishing design,adding comments,fixing some logic errors

--- calculator.py
+++ calculator.py
@@ -1,20 +1,24 @@
 import operations as op
 import os
 
-width = os.get_terminal_size().columns
+#get the center for aligning later
+width = os.get_terminal_size().columns 
 
+#current options upgradeable to a dict when i add more
 options = ["1","2","3","4"]
 
-def in_options():
+#checks if the users_input is inside options,returns the right input
+def in_options():       
     users_input = ""
-    while users_input not in options: 
-        users_input = input("\nWhat is your Choise :")
+    while users_input not in options:
+        users_input = input("\nWhat is your Choice :")
         if users_input in options:
             return users_input
         else:
             print("(",users_input,") is invalid option please chose 1,2,3...")
 
-def get_yes_no():
+#Y/N to kill the loop and exit the app
+def get_yes_no():                               
     running_input = input("Continue (y/n):").lower()
     while running_input not in ["y","n"]:
         print("Invalid input. Please enter 'y' or 'n'.")
@@ -26,34 +30,62 @@ def get_yes_no():
 def showresult(result):
     print("\nresult =",result,"\n")
 
+#gets a float value predicts a ValueError
+def get_number(prompt):   
+    while True:
+        num = input(prompt)
+        try:
+            num = float(num)
+            return num
+        except ValueError:
+            print("String :("+num+") is an invalid input")
+
+#just a menu print function
 def menu():
     print("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
-    return in_options()
 
+
+#main code
 print("CALCULATOR".center(width))
 print("\nChoose 1,2,3,4.... based on what mathematical operation you wish to be used")
 
-
+#main while. Handles the give and take
 running = True
-while running == True:     #breaks when user inputs 0
-    users_input = menu()
-    a = float(input("\nfirst number :"))
-    b = float(input("\nsecond number :"))
-    if users_input == "1":        #main if that deside's what action should be done
-        result = op.addition(a,b)
-        showresult(result)
+while running:
+    menu() #print menu
+    users_input = in_options() #gets the input,validating its on the menu
+    
+    #prints the name of the operation the user choose
+    if users_input == "1":          
+        print("\n"+"Addition:".center(width))
     elif users_input == "2":
-        result = op.subtraction(a,b)
-        showresult(result)
+        print("\n"+"Subtraction:".center(width))
     elif users_input == "3":
-        result = op.multiplication(a,b)
-        showresult(result)
-    elif users_input == "4" and b == 0: #fixes the ZeroDivisionError
-        print("\ncant divine with 0")
-        continue
+        print("\n"+"Multiplication:".center(width))
     elif users_input == "4":
-        result = op.division(a,b)
-        showresult(result)
+        print("\n"+"Division:".center(width))
+    
+    #getting our numbers
+    num1 = get_number("\nFirst number :")
+    num2 = get_number("\nSecond number :")
+    
+    #deciding what operation to use
+    result = None
+    if users_input == "1":       
+        result = op.addition(num1,num2)
+    elif users_input == "2":
+        result = op.subtraction(num1,num2)
+    elif users_input == "3":
+        result = op.multiplication(num1,num2)
+    elif users_input == "4":
+        #fixes the ZeroDivisionError
+        while num2 == 0:
+            num2 =get_number("\nInput any number but 0 :")
+        result = op.division(num1,num2)
 
+    #making sure if its an int it doesn't print as float
+    if isinstance(result, float) and result.is_integer():
+        result = int(result)
+    showresult(result)
     running = get_yes_no()
 print("THANKS!!! for using my CALCULATOR".center(width))
