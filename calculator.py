712fa7c8dbd6cd77057fbd99d725abe331f359b6
Chris Vlamis <christosvlamis1@gmail.com>
2026-03-31 21:36:25 +0300

    Add history tracking and history menu option

--- calculator.py
+++ calculator.py
@@ -4,23 +4,28 @@ import os
 #get the center for aligning later
 width = os.get_terminal_size().columns 
 
-#current options upgradeable to a dict when i add more
-options = {
-    "1": ("Addition", op.addition),
-    "2": ("Subtraction", op.subtraction),
-    "3": ("Multiplication", op.multiplication),
-    "4": ("Division", op.division)
-}
 
+def show_history():
+    if not history:
+        os.system("clear")
+        print("\nHistory is empty\n")
+        input("Press Enter to return to menu...\n")
+        os.system("clear")
+    else:
+        os.system("clear")
+        for index, item in enumerate(history):
+            print("\n"+str(index+1)+".  "+item)
+        input("Press Enter to return to menu...\n")
+        os.system("clear")
 
 #checks if the users_input is inside options,returns the right input
 def in_options():       
     while True:
-        users_input = input("\nWhat is your Choice :")
+        users_input = input("\nWhat is your Choice :").lower()
         if users_input in options:
             return users_input
         else:
-            print("(",users_input,") is invalid option please chose 1,2,3...")
+            print("(",users_input,") is invalid option please choose 1,2,3...")
 
 #Y/N to kill the loop and exit the app
 def get_yes_no():                               
@@ -53,38 +58,64 @@ def menu():
         print(key+"."+value[0])
 
 
+options = {
+    "1": ("Addition", op.addition, "+"),
+    "2": ("Subtraction", op.subtraction, "-"),
+    "3": ("Multiplication", op.multiplication, "*"),
+    "4": ("Division", op.division, "/"),
+    "h": ("History", show_history, None)
+}
+
+history = []
+
+
 #main code
 print("CALCULATOR".center(width))
 print("\nChoose 1,2,3,4.... based on what mathematical operation you wish to be used\n")
+print("Press H to view history")
 
 #main while. Handles the give and take
 running = True
 while running:
     menu() #print menu
-    users_input = in_options() #gets the input,validating its on the menu
-    name , func = options[users_input]
-    result = None
-    #getting our numbers
-    num1 = get_number("\nFirst number :")
-    num2 = get_number("\nSecond number :")
-    
-    #prints the name of the operation the user choose
-    print("\n",name.center(width))
+    users_input = in_options()#gets the input,validating its on the menu
+    name , func , symbol = options[users_input]
+    if symbol is None:
+        func()
+        continue
+    else:
+        #getting our numbers
+        num1 = get_number("\nFirst number :")
+        num2 = get_number("\nSecond number :")
+        
+        #prints the name of the operation the user choose
+        print("\n",name.center(width))
 
-    #handling errors like ZerroDivisionError
-    while True:
-        try:
-            result = func(num1, num2)
-            break
+        #handling errors like ZeroDivisionError
+        while True:
+            try:
+                result = func(num1, num2)
+                break
 
-        #i'm retaking only num2 for now ill change it with more operations than can cause more problems
-        except Exception as e:
-            print("Invalid input caused :",e,"try again :")
-            num2 =get_number("\nInput any number but 0 :")
-   
-    #making sure if its an int it doesn't print as float
-    if isinstance(result, float) and result.is_integer():
-        result = int(result)
-    showresult(result)
-    running = get_yes_no()
+            #i'm retaking only num2 for now ill change it with more operations that can cause more problems
+            except Exception as e:
+                print("Invalid input caused :",e,"try again :")
+                num2 =get_number("\nInput any number but 0 :")
+       
+        #making sure if its an int it doesn't print as float
+        if isinstance(result, float) and result.is_integer():
+            result = int(result)
+        if num1.is_integer():
+            num1 = int(num1)
+        if num2.is_integer():
+            num2 = int(num2)
+        result_log = str(num1)+" "+symbol+" "+str(num2)+" = "+str(result)
+        if len(history) >= 10:
+            history.pop(0)
+            history.append(result_log)
+        else:
+            history.append(result_log)
+
+        showresult(result)
+        running = get_yes_no()
 print("THANKS!!! for using my CALCULATOR".center(width))
